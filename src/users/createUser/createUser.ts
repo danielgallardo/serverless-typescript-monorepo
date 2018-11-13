0abe09ec941c0506eef0964501ff7fd4f8d77628
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-11-13 16:54:24 +0300

    fix services package.json

--- src/users/createUser/createUser.ts
+++ src/users/createUser/createUser.ts
@@ -5,6 +5,7 @@ type Params = {
   email: string;
 };
 
+// Use named export, it improves autocompletion
 export const createUser = async (data: Params) => {
   // Check dynogels and dynogels-promisified for mor info about Table methods
   const user = await UsersTable.createAsync(data);
