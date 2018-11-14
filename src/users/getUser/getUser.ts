6ed0a8edc0ed6347e32a58d5830de6ae8ed50e70
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-11-14 11:36:15 +0300

    check lint-staged

--- src/users/getUser/getUser.ts
+++ src/users/getUser/getUser.ts
@@ -5,6 +5,6 @@ type Params = {
 };
 
 export const getUser = async ({userId}: Params) => {
-    const user = await usersTable.getAsync(userId);
-    return user.toJSON();
+  const user = await usersTable.getAsync(userId);
+  return user.toJSON();
 };
